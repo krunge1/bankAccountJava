4336b4d5368f5a5f0c438f2375ad5e883558c586
Kyle Runge <krunge1@hotmail.com>
2023-03-15 13:50:54 -0500

    Complete with Bonus

--- BankAccount.java
+++ BankAccount.java
@@ -1,35 +1,85 @@
+import java.util.Random;
+
 public class BankAccount{
     // overall class tracked items
     public static int depositAccountsOnHand = 0;
     public static double balanceDepositAccountsOnHand = 0;
 
     // individual object attributes
+    private int accountNumber;
     private double checkingBalance;
     private double savingsBalance;
     
     //Constructer User bank accounts and initial balances
     public BankAccount (double checkingBalance, double savingsBalance){
+        this.accountNumber = randomAccountNumber();
         this.checkingBalance = checkingBalance;
         this.savingsBalance = savingsBalance;
         depositAccountsOnHand += 2;
         balanceDepositAccountsOnHand += (checkingBalance + savingsBalance);
     }
 
-//Getters//
-    //Get Checking balance and print sentence
-    public void printCheckingBalance (){
-        System.out.printf("\nYour balance in Checking $%.2f\n", checkingBalance);
+    //Random Generator for Account
+    private int randomAccountNumber(){
+        Random rand = new Random();
+        accountNumber = rand.nextInt(1000000000) + 99999999;
+        return accountNumber;
     }
-    
-    //Get Savings balance and print sentence
-    public void printSavingsBalance() {
+
+//Getters//
+    //Get account balances and print sentence
+    public void printAccountBalances (){
+        System.out.printf("\nYour balance in Checking $%.2f\n", checkingBalance);
         System.out.printf("\nYour balance in Savings $%.2f\n", savingsBalance);
     }
 
+    //Get account number and print sentence
+    public void printAccountNumber (){
+        System.out.printf("\nYour account number is %s\n", accountNumber);
+    }
 
-//Setters//
-    //Deposit based upon account
-    // public depositInAccount(String account, double amount) {
+// Setters//
+    // Deposit based upon account
+    public void depositInAccount(String account, double amount) {
+        if (account == "checking") {
+            checkingBalance += amount;
+            balanceDepositAccountsOnHand += amount;
+            System.out.printf("\nYour balance in Checking $%.2f\n", checkingBalance);
+        }
+        else if(account == "savings"){
+            savingsBalance += amount;
+            balanceDepositAccountsOnHand += amount;
+            System.out.printf("\nYour balance in Savings $%.2f\n", savingsBalance);
+        }
+        else{
+            System.out.println("You must enter savings or checking to make a deposit");
+        }
+    }
 
-    // }
+    // Withdraw based upon account and balance
+    public void withdrawInAccount(String account, double amount) {
+        if (account == "checking") {
+            if (this.checkingBalance - amount > 0) {
+                checkingBalance -= amount;
+                balanceDepositAccountsOnHand -= amount;
+                System.out.printf("\nYour balance in Checking $%.2f\n", checkingBalance);
+            }
+            else{
+                System.out.println("Insufficient funds in account");
+            }
+        }
+        else if(account == "savings"){
+            if (this.savingsBalance - amount > 0) {
+                savingsBalance -= amount;
+                balanceDepositAccountsOnHand -= amount;
+                System.out.printf("\nYour balance in Savings $%.2f\n", savingsBalance);
+            }
+            else{
+                System.out.println("Insufficient funds in account");
+            }
+        }
+        else{
+            System.out.println("You must enter savings or checking to make a deposit");
+        }
+    }
 }
--- TestBankAccount.java
+++ TestBankAccount.java
@@ -1,8 +1,37 @@
 public class TestBankAccount {
     public static void main(String[] args){
+
+        // Test class setups
+        System.out.println("*****Test Bank Account Setups******");
         BankAccount kyle = new BankAccount(5, 10);
-        kyle.printCheckingBalance();
-        kyle.printSavingsBalance();
+        BankAccount Jack = new BankAccount(55, 100);
+        
+        // Test account balance and info printing
+        System.out.println("*****Test Account Printing******");
+        kyle.printAccountBalances();
+        Jack.printAccountBalances();
+        kyle.printAccountNumber();
+        Jack.printAccountNumber();
+
+        //Test depsit functions
+        System.out.println("*****Test Deposit******");
+        kyle.depositInAccount("checking", 150);
+        kyle.depositInAccount("savings", 110);
+        kyle.depositInAccount("block", 10000);
+        Jack.depositInAccount("checking", 1500);
+        Jack.depositInAccount("savings", 200000);
+        Jack.depositInAccount("block", 10000);
+
+        //Test depsit functions
+        System.out.println("*****Test Withdraw******");
+        kyle.withdrawInAccount("checking", 50);
+        kyle.withdrawInAccount("savings", 10);
+        kyle.withdrawInAccount("block", 10000);
+        Jack.withdrawInAccount("checking", 100);
+        Jack.withdrawInAccount("savings", 200);
+        Jack.withdrawInAccount("block", 10000);
+
+        System.out.println("\n*****Test Bank Public Level Activity******");
         System.out.println(kyle.balanceDepositAccountsOnHand);
         System.out.println(kyle.depositAccountsOnHand);
     }
